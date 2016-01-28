4f482a427ae4a25c0ea0dfe1f658f06ea6608cf8
Miguel <miguel.mejia.cifuentes@gmail.com>
2016-01-28 10:01:42 +0100

    Slider en place

--- Comptoir/resources/views/layouts/slider.blade.php
+++ Comptoir/resources/views/layouts/slider.blade.php
@@ -1,10 +1,19 @@
 <div class="flexslider">
   <ul class="slides">
-    <li data-thumb="/images/resultats/thumb-slider1.jpg">
-      <img src="/images/resultats/slider1.jpg" />
+    <li data-thumb="/images/resultats/thumb-slider1.jpg ">
+      <img src="/images/resultats/slider1.jpg"/>
     </li>
     <li data-thumb="/images/resultats/thumb-slider2.jpg">
-      <img src="/images/resultats/slider2.jpg" />
+      <img src="/images/resultats/slider2.jpg"/>
+    </li>
+    <li data-thumb="/images/resultats/thumb-slider3.jpg ">
+      <img src="/images/resultats/slider3.jpg" />
+    </li>
+    <li data-thumb="/images/resultats/thumb-slider4.jpg ">
+      <img src="/images/resultats/slider4.jpg"/>
+    </li>
+    <li data-thumb="/images/resultats/thumb-slider5.jpg ">
+      <img src="/images/resultats/slider5.jpg"/>
     </li>
   </ul>
 </div>
